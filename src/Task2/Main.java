3ef56f27b4ebbbd344f307e654e3c3a39cbde4ce
orewka <orewka@inbox.ru>
2019-11-16 17:48:47 +0300

    2

---
+++ src/Task2/Main.java
@@ -0,0 +1,28 @@
+package Task2;
+
+import java.io.FileNotFoundException;
+import java.nio.file.AccessDeniedException;
+import java.util.jar.JarException;
+
+public class Main {
+    static void throwException(Status status) throws JarException, FileNotFoundException, AccessDeniedException {
+        if (status.equals(Status.FILE_NOT_FOUND)) throw new FileNotFoundException();
+        if (status.equals(Status.ACCESS_DENIED)) throw new AccessDeniedException("Что тут?");
+        if (status.equals(Status.JAR_ERROR)) throw new JarException();
+
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            throwException(Status.FILE_NOT_FOUND);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (AccessDeniedException e) {
+            e.getMessage();
+            throw new ???;
+        } catch (JarException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Task2/Status.java
@@ -0,0 +1,5 @@
+package Task2;
+
+public enum Status {
+    FILE_NOT_FOUND, ACCESS_DENIED, JAR_ERROR
+}
